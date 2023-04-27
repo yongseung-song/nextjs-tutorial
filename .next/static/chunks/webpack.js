dc2560d159496a64af2deb3f9ba31ff77daa57e9
Yongseung Song <yongseung.song@icloud.com>
2023-04-27 16:06:23 +0900

    go to next tutorial.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba27d828f6a34b4a"; }
+/******/ 		__webpack_require__.h = function() { return "95b515770fc69211"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ function Header({ title }) {
 export default function HomePage() {
   const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
   const [likes, setLikes] = useState(0);
+
   function handleClick() {
     setLikes(likes + 1);
   }
